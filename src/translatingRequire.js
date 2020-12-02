63d1e233bff726ff612a99d64712a987d736395d
Mohammed Samir <ms.2052001@gmail.com>
2020-12-02 12:08:52 +0200

    Release: update V1.0

--- src/translatingRequire.js
+++ src/translatingRequire.js
@@ -1,11 +1,12 @@
 var __old__require__ = require;
-var __require_cache__ = {};
+var __require__cache__ = {};
 var __modules__tmap__ = MODULES_TRANSLATION_MAP;
 require = function(m) {
-  if(m in __require_cache__) return __require_cache__[m];
+  if(m in __require__cache__) return __require__cache__[m];
   if (m in __modules__tmap__) {
-    _ = __arjs__translate__(__old__require__(m), __modules__tmap__[m]);
-    Object.assign(__require_cache__, m, { value: _ });
+    let _m = __modules__tmap__[m];
+    let _ = __arjs__translate__(__old__require__(_m[0]), __modules__tmap__[_m[1]]);
+    Object.assign(__require__cache__, m, { value: _ });
     return _;
   }
   return __old__require__(m);
