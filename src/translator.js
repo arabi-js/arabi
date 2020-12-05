ff38ff789ef54d5e9a570f77effeb03f9bf91f49
Mohammed Samir <ms.2052001@gmail.com>
2020-12-05 21:11:43 +0200

    Translate function's result as well

--- src/translator.js
+++ src/translator.js
@@ -3,6 +3,7 @@ function __arjs__translate__(o, m) {
     get(target, prop) {
       if (prop in target) return target[prop];
       let v = m[prop];
+      if (v)
       if (typeof v === 'string') {
         Object.defineProperty(target, prop, { value: target[v] });
         return target[v];
@@ -11,11 +12,19 @@ function __arjs__translate__(o, m) {
         let _v = __arjs__translate__(target[v[0]], v[1]);
         Object.defineProperty(target, prop, { value: _v });
         return _v;
-      } else if (v) {
+      } else if (v === true) {
         // we translate the result of a function
-        let _v = () => __arjs__translate__(target[v[0]](...arguments), v[1]);
+        let _v = function(){ return __arjs__translate__(target[v[0]](...arguments), v[1]) };
         Object.defineProperty(target, prop, { value: _v });
         return _v;
+      } else if (typeof v[2] === 'object') {
+        // translate the function with the first map, and the function's result with the other.
+        let _v = function(){ return __arjs__translate__(target[v[0]](...arguments), v[2]) };
+        _v = __arjs__translate__(_v, v[1]);
+        Object.defineProperty(target, prop, { value: _v });
+        return _v;
+      } else {
+        throw "unexpected translation map!\nwe accept only one of these three arrays whose first element is the actual prop name, the second is object if you are translating object, if you translating the result of a function then the 3rd is (true), if you want to translate the function and translate also its results you have to pass the second elemet of the array to be the function tmap, and the second as the function's result tmap."
       }
       return undefined;
     },
