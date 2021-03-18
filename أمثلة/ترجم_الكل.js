bba23d9196a0e515d29d58ef34f89f7d7deb4776
Mohammed Samir <ms.2052001@gmail.com>
2021-03-18 17:53:58 +0200

    إعادة تسمية تجميع إلى ترجم في ./أمثلة

--- أمثلة/ترجم_الكل.js
+++ أمثلة/ترجم_الكل.js
@@ -4,7 +4,7 @@ const path = require("path");
 let examples =
   fs.readdirSync(__dirname, { withFileTypes: true })
   .filter(p=>p.isDirectory())
-  .map(d=>path.resolve(__dirname, d.name, 'تجميع'));
+  .map(d=>path.resolve(__dirname, d.name, 'ترجم'));
 
 for(let [i, p] of examples.entries()) {
   console.log(i + 1 + " " + "*".repeat(10));
