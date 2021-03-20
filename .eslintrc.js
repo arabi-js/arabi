25c52b53209b22dfc180b6ef6e02a58cb110ff0c
Mohammed Samir <ms.2052001@gmail.com>
2021-03-21 00:36:05 +0200

    تعديلات وتحسينات

---
+++ .eslintrc.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const cjsGlobals = ['__dirname', '__filename', 'require', 'module', 'exports'];
+
+const flowPackages = ['حزم/{لب,محلل}/مصدر/**/*.js'];
+
+module.exports = {
+  root: true,
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended'],
+  overrides: [
+    {
+      files: flowPackages,
+      parser: '@babel/eslint-parser', // to enable flowjs syntax to be parsed
+      parserOptions: { sourceType: 'module' },
+      plugins: ['flowtype'],
+      extends: ['eslint:recommended', 'plugin:flowtype/recommended'],
+      settings: {
+        flowtype: {
+          onlyFilesWithFlowAnnotation: false,
+        },
+      },
+    },
+    {
+      files: ['حزم/مصدر/**/*.js'],
+      rules: {
+        'no-restricted-globals': ['error', ...cjsGlobals],
+      },
+    },
+    {
+      files: ['حزم/محلل/**/*.js'],
+      // @babel/eslint-config-development-internal rules
+      rules: {
+        '@babel/development-internal/dry-error-messages': [
+          'error',
+          {
+            errorModule: path.resolve(__dirname, 'حزم/محلل/مصدر/parser/error.js'),
+          },
+        ],
+      },
+    },
+    {
+      files: ['حزم/ترجم/مصدر/{استدعي,مدخل}.js'],
+      rules: {
+        'no-undef': 0,
+      },
+    },
+  ],
+};
