4b7e17723b1bae8aa75d07ca896a94ad893db7b0
Mohammed Samir <ms.2052001@gmail.com>
2021-03-25 21:57:44 +0200

    وضع المعتمدات في أماكنها الصحيحة

--- أمثلة/4.لوحة-لغة-الوسوم-التشعبية/babel.config.js
+++ أمثلة/4.لوحة-لغة-الوسوم-التشعبية/babel.config.js
@@ -1,9 +1,7 @@
 module.exports = {
   "presets": ["@babel/env"],
   "plugins": [
-    "@babel/transform-runtime",
-    "@babel/transform-spread",
     "@babel/proposal-private-methods",
-    "@babel/proposal-class-properties",
+    "@babel/proposal-class-properties"
   ]
 }
